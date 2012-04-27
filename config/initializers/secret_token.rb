ae93a452e9767b6c0a289e95498ab1a8542328c3
hava <hava@bright-sun.org>
2012-04-27 17:01:28 +0200

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sightings::Application.config.secret_token = 'cdf29e1bb21f53aacac4af89127aaa3f3e0f78c13e4bc664c0fffaeb67481c8cdac82a629c8e4cf3f12423adcde9b06b9760c6f2a239f562b8836f0587642906'
